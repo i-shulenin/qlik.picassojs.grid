8b8250423efaaa5bb2119730af15f55280d30100
Ilya Shulenin <>
2021-05-03 10:26:46 +0000

    Fixed report.component

--- view/report/report.component.js
+++ view/report/report.component.js
@@ -5,23 +5,9 @@ angular.
   component('report', {
     templateUrl: 'view/report/report.template.html',
     controller: ['$routeParams', 'Report',
-      async function reportController($routeParams, Report) {
+      function reportController($routeParams, Report) {
         this.qDocId = $routeParams.qDocId;
         this.qId = $routeParams.qId;
-
-        this.layout = await Report.get({
-          command: 'sheet',
-          app: this.qDocId,
-          sheet: this.qId,
-        }).$promise
-          .then((result) => {
-
-            return result.cells;
-          })
-          .catch((error) => {
-            console.log('%c*** error ***', 'background: #FF0000; color: #FFFFFF; font-weight: bold;');
-            console.log(error);
-          });
       }
     ]
   });
